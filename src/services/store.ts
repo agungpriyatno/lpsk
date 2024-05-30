a000852ee3073bb3ba0a9c75226de117de2e8ed5
agungpriyatno <agungpriyatno303@gmail.com>
2024-05-30 10:10:44 +0700

    update

--- src/services/store.ts
+++ src/services/store.ts
@@ -5,5 +5,5 @@ import { storeObject } from "@/lib/bucket";
 export const store = async (formData: FormData): Promise<string> => {
   const file = formData.get("file") as File;
   const filename = await storeObject(file, "publikasi");
-  return `${process.env.BUCKET_URL_ACCESS}/publikasi/${filename}`;
+  return `${process.env.BUCKET_URL_ACCESS}/${filename}`;
 };
